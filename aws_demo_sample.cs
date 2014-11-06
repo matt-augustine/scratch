8da75de449bcf9a85c245bf20e49eae5ec3fe895
Matt Augustine <matt@playfab.com>
2014-11-06 15:08:28 -0800

    Update aws_demo_sample.cs

--- aws_demo_sample.cs
+++ aws_demo_sample.cs
@@ -3,19 +3,31 @@
     ReadStoreOrder =    new[] { DataStore.DynamoDB, DataStore.SimpleDB }, 
     WriteRegion =       "us-west-2", 
     ReadRegionOrder =   new[] { "us-west-2", "us-west-1" }
-                                                                       )]
+ )]
 public class Player : PlayFabData<Player, UInt64>
 {
     [Amazon.DynamoDBv2.DataModel.DynamoDBHashKey]
-    public UInt64   UberId { get; set; }
-    public string   Email { get; set; }
-    public DateTime Created { get; set; }
+    public UInt64   UserId    { get; set; }
+    public string   Email     { get; set; }
+    public DateTime Created   { get; set; }
     public DateTime LastLogin { get; set; }
 
     static void Sample()
     {
+        // Returns the first matching player record, searching
+        // DynamoDB and SimpleDB in both regions.
         var player = Player.Get(1234, false);
         player.LastLogin = DateTime.UtcNow;
+        
+        // Updates the player record in the store and 
+        // region from which it was loaded.
         player.Update();
+        
+        // Creates a player record in the default store and region
+        var newPlayer = Player.Create(new Player()
+        {
+            UserId= 7329,
+            Created = DateTime.UtcNow,
+        })
     }
 }
